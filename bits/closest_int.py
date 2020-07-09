daabb0ab2e2abc5f81df65f676512d8df0ed05f2
stgleb <glebstepanov1992@gmail.com>
2020-07-09 20:17:45 +0300

    Add find closest bit

--- bits/closest_int.py
+++ bits/closest_int.py
@@ -8,14 +8,15 @@ def swap_bits(n, i, j):
 # Find closest integer number with same count of bits set to 1
 def closest(n):
     i = 0
-    while 1 << i < n:
-        if (1 << i + 1) & n != (1 << i) & n:
+    while 1 << i + 1 <= n:
+        if n >> i + 1 & 1 != n >> i & 1:
             return swap_bits(n, i + 1, i)
         i += 1
-    return n
+    return n >> 1 | n + 1
 
 
 if __name__ == "__main__":
     print(closest(5))
     print(closest(17))
     print(closest(10))
+    print(closest(15))
